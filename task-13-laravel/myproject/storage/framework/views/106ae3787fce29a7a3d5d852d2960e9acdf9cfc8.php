7e4c64bce7efdc1d5a3a90f483a658ebab1cec59
suraj <suraj.khari@appfoster.com>
2024-03-22 13:07:01 +0530

    chnages

---
+++ task-13-laravel/myproject/storage/framework/views/106ae3787fce29a7a3d5d852d2960e9acdf9cfc8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+</head>
+    <body>
+        <h1 style="text-align:center;">This is a WEB APP </h1>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\myproject\resources\views/welcome.blade.php ENDPATH**/ ?>
